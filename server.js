85e334d59330bc494a2ed120ac3a2645758a9d8d
RockyWearsAHat <alexwaldmann2004@gmail.com>
2024-01-30 20:13:41 -0700

    base code for rendering out a view on the index layout

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+
+const app = express();
+
+//Set up handlebars
+const hbs = handlebars.create();
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("layouts", process.cwd() + "/views/layouts");
+
+app.get("*", (req, res) => {
+  res.render("homepage", { layout: "index", title: "testing 123" });
+});
+
+app.listen(3000, () =>
+  console.log("App is listening on http://localhost:3000")
+);
