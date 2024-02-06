8e4c1a969fe83fd3dcefe2a5a58f9d13e0c3a53e
Snowbirdlam <joanna.lam@du.edu>
2024-02-05 18:40:13 -0700

    added model and seed data

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const sequelize = require("./db/sequelizeConn");
-const User = require("./models/user");
+const User = require("../models/user");
 
 const app = express();
 
