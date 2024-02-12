fcbc69d6875278223c1f7486acacecfe2b9b5475
Snowbirdlam <joanna.lam@du.edu>
2024-02-12 05:06:06 -0700

    added get routers, images to seed data

--- routes/conLepo.js
+++ routes/conLepo.js
@@ -3,12 +3,14 @@ const Lepo = "../../models/Lepo";
 
 routeHandler.get("/:Name", async (req, res) => {
   try {
-    const butterfly = await Lepo.findOne({ raw: true, where: {Name:req.query.Name} });
-   if (butterfly == null||!butterfly||butterfly == undefined) throw new Error ("Not in Database!");  
-   res.json(butterfly);
+    const butterfly = await Lepo.findOne({ raw: true, where: { Name: req.query.Name } });
+    if (butterfly == null || !butterfly || butterfly == undefined) throw new Error("Not in Database!");
+    res.json(butterfly);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 });
 
 module.exports = routeHandler;
+
+
