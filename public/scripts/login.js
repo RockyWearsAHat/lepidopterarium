7cc08d1716d01bc3b60df0133f8aa24b64037b4e
RockyWearsAHat <alexwaldmann2004@gmail.com>
2024-02-12 18:24:57 -0700

    Heroku

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -18,7 +18,7 @@ const handleSubmit = async (e) => {
     if (!username || username == "" || !password || password == "")
       throw new Error("Please enter a valid username and password");
 
-    const res = await fetch("http://localhost:3000/api/user/login", {
+    const res = await fetch("/api/user/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
